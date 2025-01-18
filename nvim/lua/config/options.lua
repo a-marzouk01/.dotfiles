6e9460eed8f7f3ac99df76a97dbce986502b9f51
a-marzouk01 <a.marzouk01@outlook.com>
2025-01-18 04:43:02 +0200

    new vim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,26 @@
+local set = vim.opt
+
+set.guicursor = ""
+
+set.number = true
+set.relativenumber = true
+
+set.smartindent = true
+
+vim.opt.undofile = true
+
+set.wrap = false
+
+set.hlsearch = false
+set.incsearch = true
+
+set.termguicolors = true
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
